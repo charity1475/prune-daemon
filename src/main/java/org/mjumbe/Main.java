0695572687b24d52de45c1a66eebeb14a3883d5b
Charity <charitymbisi@gmail.com>
2025-05-10 19:43:29 +0300

    fix: skip saving when no data is retrieved from cache

--- src/main/java/org/mjumbe/Main.java
+++ src/main/java/org/mjumbe/Main.java
@@ -5,6 +5,7 @@ import org.mjumbe.store.RedisClient;
 import org.mjumbe.transform.ResizeTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.time.LocalDateTime;
 
 public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -19,18 +20,21 @@ public class Main {
 			try (RedisClient dataCache = new RedisClient(config.redisHost, config.redisPort, config.redisPassword)) {
 				String data = dataCache.queryCache(config.redisKey);
 				logger.info("Data successfully fetched from : {}", config.redisKey);
+				if (data == null || data.isEmpty()) {
+					logger.warn("No data found in Redis for key: {} : {}", config.redisKey, data);
+					return;
+				}
 
 				String prunedData = transformer.prune(data);
 				logger.info("Data successfully pruned cache");
-				if (prunedData != null || !("[]".equals(prunedData))) {
-					dataCache.cacheData(config.redisKey, prunedData);
-				}
+
+				dataCache.cacheData(config.redisKey, prunedData);
 				logger.info("Data successfully updated in Redis with key: {}", config.redisKey);
 			}
 		} catch (Exception e) {
 			logger.error("An error occurred during the scheduled job execution: {}", e.getMessage());
 			System.exit(1);
 		}
-		logger.info("Cache prune job completed successfully at {} ...", java.time.LocalDateTime.now());
+		logger.info("Cache prune job completed successfully at {} ...", LocalDateTime.now());
 	}
 }
